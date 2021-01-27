b859a6687cc6cad4b95e95e63d2afa770f8dcdd9
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2021-01-27 22:04:26 +0700

    Add files via upload

---
+++ ltc.sh
@@ -0,0 +1,15 @@
+pkg install python
+pkg install lolcat
+pkg install toilet
+pkg install curl
+pkg install nano
+pkg install php
+pip install requests
+pip install mechanize
+pip install colorama
+pip install bs4
+pip install async_generator
+pip install telethon
+pip install pyaes
+pkg install figlet
+python ltc.py
